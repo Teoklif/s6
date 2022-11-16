43dc305a6189d1e5a3b328725a8b3256585ad30a
alpryakh <alpryakh@gmail.com>
2022-11-16 17:53:50 +0300

    ex 41

---
+++ ex 41/Program.cs
@@ -0,0 +1,42 @@
+﻿Console.Clear();
+int[] array = CreateArray(EnterLength());
+ShowArray(array);
+NumberSum();
+
+int EnterLength()
+{
+    Console.Write("Enter your array's length:");
+    int length = int.Parse(Console.ReadLine());
+    return length;
+}
+
+void NumberSum()
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+            sum++;
+
+    }
+
+    Console.Write($"{sum}");
+}
+
+int[] CreateArray(int size)
+{
+    Console.WriteLine("Введите количество элементов массива");
+    int[] massive = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        Console.WriteLine($"Введите {i + 1} элемент массива");
+        massive[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return massive;
+}
+
+void ShowArray(int[] array)
+{
+
+    Console.WriteLine($"[ {String.Join(", ", array)} ] => ");
+}
