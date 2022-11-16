962627083bd3a25eb55b5c2fd9724e8a5399cfd2
alpryakh <alpryakh@gmail.com>
2022-11-16 17:54:17 +0300

    exx 43

---
+++ ex 43/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+
+Console.Write("Enter k1:");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter b1:");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter k2:");
+double k2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter b2:");
+double b2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine($"({FindX()}; {FindY()})");
+double FindX()
+{
+    double x = (b2-b1)/(k1-k2);
+    return x;
+}
+double FindY()
+{
+    double y = k1*(b2-b1)/(k1-k2) + b1;
+    return y;
+}
